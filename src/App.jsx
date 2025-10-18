73c295d6c9350d36892deb18939144b08c39f120
Dathan Towns <dathantowns@gmail.com>
2025-10-18 10:55:36 -0700

    feat: add very basic scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="app">app</div>
+      <div className="app">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     </>
   );
 }
